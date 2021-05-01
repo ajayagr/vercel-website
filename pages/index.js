eb3c5c5b03e059a3b6262a40bd9f287e243efdcd
Ajay Kumar Agarwal <ajay.agr11@gmail.com>
2021-05-01 21:00:14 +0530

    testing preview branch

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  //Hello world!
-  let envName = process.env.NEXT_PUBLIC_MY_NAME;
-  // console.log(envName);
-  return <h2>Hello World!!</h2>;
+  return (
+    <>
+      <Head>
+        <meta name="description" content="Ajay Agarwal, Frontend Developer, Upcoming UI/UX Designer" />
+        <title>Ajay Agarwal</title>
+      </Head>
+      <h2>Hello World!!</h2>
+    </>
+  );
 }
